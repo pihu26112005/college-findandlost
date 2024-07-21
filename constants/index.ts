a7b4af04141947e64d713b0f425fa105fe85c98d
pihu26112005 <161943145+pihu26112005@users.noreply.github.com>
2024-07-22 00:51:34 +0530

    lib public constants

---
+++ constants/index.ts
@@ -0,0 +1,31 @@
+export const SidebarLinks = [
+    {
+        label: 'Home',
+        route: '/',
+        imageUrl: 'icons/Home.svg'
+    },
+    {
+        label: 'Find Query',
+        route: '/allFindQuery',
+        imageUrl: 'icons/upcoming.svg'
+    },
+    {
+        label: 'Lost Query',
+        route: '/=allLostQuery',
+        imageUrl: 'icons/previous.svg'
+    },
+    {
+        label: 'My Query',
+        route: '/myQuery',
+        imageUrl: 'icons/Video.svg'
+    },
+
+]
+
+export const avatarImages = [
+    '/images/avatar-1.jpeg',
+    '/images/avatar-2.jpeg',
+    '/images/avatar-3.png',
+    '/images/avatar-4.png',
+    '/images/avatar-5.png'
+]
