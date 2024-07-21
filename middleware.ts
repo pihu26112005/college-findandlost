b29f9357fa9628e9c553fc1e5a13a720acb43b40
pihu26112005 <161943145+pihu26112005@users.noreply.github.com>
2024-07-22 01:03:31 +0530

    Add files via upload

---
+++ middleware.ts
@@ -0,0 +1,18 @@
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+
+const isProtectedRoute = createRouteMatcher([
+  '/',
+  '/allFindQuery',
+  '/allLostQuery',
+  'myQuery',
+  '/query(.*)'
+]);
+
+export default clerkMiddleware((auth, req) => {
+  if (isProtectedRoute(req)) auth().protect();
+});
+
+
+export const config = {
+  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+};
